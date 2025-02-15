ca6383810dd0c0e7dd77d34205935a248e5bfad3
ParmS-Musale <musaleparm9541@gmail.com>
2025-02-15 17:48:47 +0530

    implement todo list functionality with add feature and dynamic rendering

--- Week 06/src/App.jsx
+++ Week 06/src/App.jsx
@@ -1,30 +1,43 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <HeaderWithButton />
-      <Header title="Parm Is here"></Header>
-      <Header title="Parm is Went"></Header>
-    </>
-  );
-}
+let count = 4;
 
-function HeaderWithButton() {
-  const [title, setTitle] = useState("Parm");
-  function updateTitle() {
-    setTitle(Math.random());
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "Learn React", description: "Learn React" },
+    { id: 2, title: "Learn Firebase", description: "Learn Firebase" },
+    { id: 3, title: "Learn Node.js", description: "Learn Node.js" },
+  ]);
+
+  function addTodo() {
+    setTodos([
+      ...todos,
+      { id: count++, title: Math.random(), description: Math.random() },
+    ]);
   }
   return (
     <>
-      <button onClick={updateTitle}>click me to chnage the tittle</button>
-      <Header title={title}></Header>
+      <div>
+        <button onClick={addTodo}>Add a Todo</button>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            title={todo.title}
+            description={todo.description}
+          />
+        ))}
+      </div>
     </>
   );
 }
 
-function Header({ title }) {
-  return <div>{title}</div>;
+function Todo({ title, description }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
 }
 export default App;
