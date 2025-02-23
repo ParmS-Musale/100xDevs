a17701eada592b97f3c313ebbc2d41bf0222221e
Parmeshwar Musale <152637541+ParmS-Musale@users.noreply.github.com>
2025-02-23 19:50:56 +0530

    Update App.jsx

--- Week 05/src/App.jsx
+++ Week 05/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div>
-      <h1>CountGo</h1>
+      <h1>CountGoES</h1>
       <button onClick={() => setCount(count + 1)}>Increment {count}</button>
     </div>
   );
